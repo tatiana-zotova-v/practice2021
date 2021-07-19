c1d3a4ccdf4d72bbcae1ebc75ac489f7df38f009
tanyusha <zotovvvat@yandex.ru>
2021-07-19 18:19:33 +0300

    main func

---
+++ Operator/Cpp/main.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+#include "Msqtt.h"
+#include <opencv2/core.hpp>
+#include "recognition.h"
+#include "interface.h"
+
+int main()
+{
+	Operator RobotControl("Operator", "127.0.0.1", 1883, 1, 2);
+
+	MainWindow window;
+
+	window.MainCycle(RobotControl);
+
+	return 0;
+}
